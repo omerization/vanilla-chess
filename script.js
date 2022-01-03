1095aa377b2e699d40a6e719a078b9e36da64106
omerization <omerkaanaslan@gmail.com>
2022-01-03 17:31:46 +0300

    comments added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//function that takes players names and prints them
 const welcomer = () => {
     let whitePlayer = prompt("Name of white player", "White Player");
     let blackPlayer = prompt("Name of black player", "Black Player");
@@ -11,20 +12,22 @@ const welcomer = () => {
     }
 }
 
+// Invoke the welceme function when page is loaded
 welcomer();
 
-// Function 
+// Function that is going to be used when drag is started. Save the data of offset and data-item attributes
 const drag_start = (event) => {
     var style = window.getComputedStyle(event.target, null);
     event.dataTransfer.setData("text/plain", (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY) + ',' + event.target.getAttribute('data-item'));
 }
 
-
+// Function that is going to be used when element is dragging, doing nothing basically.
 const drag_over = (event) => {
     event.preventDefault();
     return false;
 }
 
+// Function that is going to be used when element is dropped somewhere, changes the position of the element to where its dropped.
 const drop = (event) => {
     var offset = event.dataTransfer.getData("text/plain").split(',');
     var piece = document.querySelectorAll(`[data-item='${(offset[2])}']`)
@@ -34,6 +37,7 @@ const drop = (event) => {
     return false;
 }
 
+// adding event listener for all elements of pieces collection
 var pieces = document.getElementsByClassName('piece');
 for (var i = 0; i < pieces.length; i++) {
     pieces[i].addEventListener('dragstart', drag_start, false);
